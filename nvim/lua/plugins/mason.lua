ec8cc54f8797d38dcac533877eca02265d0bac9a
AntTra <anton.tran@live.no>
2026-08-13 01:41:03 +0200

    enabled astro lsp, mason, treesitter and autocomplete and tools for python and cpp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason
 
 ---@type LazySpec
@@ -13,12 +11,22 @@ return {
       ensure_installed = {
         -- install language servers
         "lua-language-server",
+        "clangd", -- C/C++ (ROS2 nodes)
+        "rust-analyzer", -- Rust (ROS2 nodes)
+        "pyright", -- Python (ROS2 nodes)
+        "cmake-language-server", -- CMakeLists.txt
+        "lemminx", -- XML (package.xml, launch files)
+        "yaml-language-server", -- params/config YAML
 
         -- install formatters
         "stylua",
+        "clang-format", -- respects a repo's .clang-format
+        "black",
+        -- rustfmt is not a Mason package; it ships with rustup/rust-analyzer
 
         -- install debuggers
         "debugpy",
+        "codelldb", -- C++ node debugging
 
         -- install any other package
         "tree-sitter-cli",
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -9,6 +7,14 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "cpp",
+      "c",
+      "python",
+      "cmake",
+      "xml",
+      "yaml",
+      "bash",
+      "rust",
       -- add more arguments for adding more treesitter parsers
     },
   },
